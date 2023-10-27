887bf935062ab20abab76a578e28fa90df0aa616
TeroJii <78660782+TeroJii@users.noreply.github.com>
2023-10-27 17:04:49 +0300

    start testing maze building using copilot

---
+++ R/build_maze.R
@@ -0,0 +1,17 @@
+## A script to build a maze
+## contains a function called build_maze, which takes the following inputs
+## nrow: number of rows in the maze
+## ncol: number of columns in the maze
+## start: the starting point of the maze
+## end: the ending point of the maze
+## and returns a plotted image of a maze showing the maze walls with the following properties
+## 1. The maze is a matrix with nrow rows and ncol columns
+## 2. The maze has a start point and an end point
+## 3. The maze has a unique solution
+## 4. The maze has no loops
+## 5. The maze has no isolated points
+## 6. The maze has no inaccessible points
+## 7. The maze has no inaccessible areas
+
+
+# using Iterative randomized Kruskal's algorithm for creating the maze
